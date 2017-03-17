9c75eb3566f4418b60f7458ebe1dfaf92ad9390b
Steve Van Hooser <vanhoosr@brandeis.edu>
2017-03-17 14:24:21 -0400

    updates to intrinsic code

--- AnalysisTools/intrinsic/plotintrinsic_images_figures.m
+++ AnalysisTools/intrinsic/plotintrinsic_images_figures.m
@@ -8,6 +8,9 @@ assign(varargin{:});
 z = loadStructArray([base_directory filesep 'sf_responses.txt']);
 z = z(animal_number);
 
+Sfdat= intrinsic_mouse_md_sf_analysis2(base_directory, animal_number);
+
+
 f1 = figure;
 %this will plot all four images of both eyes, and both hemispheres
 
@@ -38,66 +41,49 @@ end;
 % first plots (1) ODI using sf 0.05, then (2) right hemisphere, left eye,
 % then (3) is right hemisphere, rigght eye, and (4) is left  hemisphere,
 % left eye, ALL 3 plotted along log sf
-% is 6, 8, 2
 
-ODstruct = intrinsic_mouse_od_analysis_v2_5('V:\Projects\ChelseaISI\2017-01-19', 't00003', 't00002');
+
+sf_resp_info = loadStructArray([base_directory filesep 'sf_responses.txt']);
+
+ODstruct = intrinsic_mouse_od_analysis_v2_5(base_directory, sf_resp_info.CONTRAHEM_CONTRAEYE, sf_resp_info.CONTRAHEM_IPSIEYE);
 f2 = figure;
 subplot(2,2,1);
 bar(ODstruct.odindex);
+axis([0 2 -1 1])
 title('Ocular Dominance Index')
 hold on;
 
 % set the indices for null, rhem leye, rhem reye, lhem leye
 indices = [0, 6, 8, 2]
+
+condition_names = {'', 'CONTRAHEM_IPSIEYE', 'CONTRAHEM_CONTRAEYE', 'IPSIHEM_IPSIEYE'};
+
 titles = {'', 'Contra Hem, Deprived Eye', 'Contra Hem, Non-Deprived', 'Ipsi Hem, Deprived'}
 
 % Analyze the data
-Sfdat= intrinsic_mouse_md_sf_analysis2('V:\Projects\ChelseaISI\2017-01-19', 1)
 
 for i=2:4,
     subplot(2,2,i);
-    x_sfs=Sfdat(indices(i)).sfs;
-    y_response=Sfdat(indices(i)).sf_responses;
+    myindex = -1;
+    for j=1:length(Sfdat),
+        if strcmp(Sfdat(j).condition_name,condition_names{i}) & strcmp(Sfdat(j).roi_name,'roi_monoV1'),
+            myindex = j;
+        end;
+    end;
+    if myindex==-1, error(['did not find a match.']); end;
+    x_sfs=Sfdat(myindex).sfs;
+    y_response=Sfdat(myindex).sf_responses;
     scatter(x_sfs,y_response);
     set(gca,'xscale','log');
     title(titles(i));
+    box off;
+    
+    xlabel('Spatial Frequencies');
+    ylabel('Response');
+    title('');
+    
     hold on;
-    %dirname = getfield(z,fn{i});
-    %stimlist = load([base_directory filesep dirname filesep 'stims.mat'],'-mat');
-
-    %for n=2,
-        %stim = get(stimlist.saveScript,n);
-        %s = load([base_directory filesep dirname filesep 'singlecondition' sprintf('%.4d',n) '.mat']);
-        
-     %   title
-        
-        
-      %  title(fn{i});
-       % hold on;
-        %plot(bino.xi,bino.yi,'b-');
-        
-    %end;
 end;
-
-
-
-
-
-subplot(2,2,1)
-
-
-subplot(2,2,2)
-plot(ans(1).sfs,ans(1).sf_responses,'og')
-hold on;
-plot(ans(2).sfs,ans(2).sf_responses,'ob')
-
-xlabel('Spatial Frequencies');
-ylabel('Response');
-title('');
-
-
-
-hold on; % leave the current plot on the figure
 
 
 
