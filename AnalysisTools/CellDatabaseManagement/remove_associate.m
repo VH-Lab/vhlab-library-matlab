1aacb3ee694e769b2bedb359e628ad42e4e4614d
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-05-10 11:00:38 -0400

    added capability for more than one associate to be removed simultaneously from remove_associate.m

--- AnalysisTools/CellDatabaseManagement/remove_associate.m
+++ AnalysisTools/CellDatabaseManagement/remove_associate.m
@@ -6,18 +6,29 @@ function remove_associate(ds, type)
 %  Removes the associate with type TYPE from all experiment variables
 %  in the dirstruct DS. If any changes are made, then the experiment variables
 %  are saved back to disk.
+%
+%  TYPE may be a single string or a cell array of strings, if more than one 
+%  associate type is to be removed.
+%
+%  See also: ASSOCIATE, SAVEEXPVAR
 % 
 
 [vars,varnames]=load2celllist(getexperimentfile(ds),'*','-mat');
 
 changemade = 0;
 
+if isa(char,type),
+	type = {type};
+end;
+
 for i=1:length(vars),
 	if isa(vars{i},'measureddata'),
-		[a,i]=findassociate(vars{i},type,'','');
-		if ~isempty(a),
-			vars{i} = disassociate(vars{i},i);
-			changesmade = 1;
+		for j=1:length(type),
+			[a,i]=findassociate(vars{i},type{j},'','');
+			if ~isempty(a),
+				vars{i} = disassociate(vars{i},i);
+				changesmade = 1;
+			end;
 		end;
 	end;
 end;
