a3d58d4fec06fa11097baa1b072ca543c8db2582
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-03-24 15:58:00 -0400

    small bug fix to cellname2nameref

--- AnalysisTools/CellDatabaseManagement/cellname2nameref.m
+++ AnalysisTools/CellDatabaseManagement/cellname2nameref.m
@@ -15,9 +15,9 @@ function [nameref,index,datestr] = cellname2nameref(cellname)
 %   See also: NAMEREF2CELLNAME, CELLNAME2DATE
 
 m = find(cellname=='_');
-nameref = struct('name',cellname(m(1)+1:m(end-5)-1), ...
-	'ref',round(str2num(cellname(m(end-4)+1:m(end-3)-1))));
+nameref = struct('name',cellname(m(1)+1:m(2)-1), ...
+	'ref',round(str2num(cellname(m(2)+1:m(3)-1))));
 
-index = round(str2num(cellname(m(end-3)+1:m(end-2)-1)));
+index = round(str2num(cellname(m(3)+1:m(4)-1)));
 
 datestr = cellname(m(end-2)+1:end);
